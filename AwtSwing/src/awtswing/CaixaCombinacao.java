83e71b18095175189bfff7df741a46b7696b6f5a
rtorraca <robson.torraca@gmail.com>
2015-08-12 17:17:53 -0400

    Envia arquivo

---
+++ AwtSwing/src/awtswing/CaixaCombinacao.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package awtswing;
+
+/**
+ *
+ * @author rtorraca
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CaixaCombinacao extends JFrame {
+    
+    JList lista;
+    String linguagens[]={"Campo Grande","Cuiabá","Goiânia"};
+    
+    public CaixaCombinacao(){
+        
+        super("Uso da classe JComboBox");
+        Container tela = getContentPane();
+        FlowLayout Layout = new FlowLayout();
+        
+        tela.setLayout(Layout);
+        Tratador trat = new Tratador();
+        lista = new JComboBox(linguagens);
+        lista.setMaximumRowCount(5);
+        JButton exibirBtn = new JButton("Exibir valor Selecionado");  
+        exibirBtn.addActionListener(trat);
+        tela.add(lista);
+        tela.add(exibirBtn);
+        setSize(300,100);
+        setVisible(true);
+               
+    }
+    
+    public static void main(String args){
+        CaixaCombinacao app = CaixaCombinacao();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              
+    }
+    
+    private class Tratador implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+        String escolha = linguagens[lista.getSelectedIndex()];
+        JOptionPane.showMessageDialog(null,"Linguagem"+escolha);
+    }
+}
+    
+}
